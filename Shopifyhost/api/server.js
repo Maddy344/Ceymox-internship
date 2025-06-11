cf5052fd9ea48f8d56def002303a8a8628806dca
Maddyvampire123 <vampirepes24@gmail.com>
2025-06-11 11:02:54 +0530

    frontend url to vercel

--- Shopifyhost/api/server.js
+++ Shopifyhost/api/server.js
@@ -50,11 +50,11 @@ async function getConnection() {
   if (!pool) {
     // Hardcode the connection details directly
     pool = mysql.createPool({
-      host: 'maglev.proxy.rlwy.net',
-      port: 24081,
-      user: 'root',
-      password: 'klArdGXdTUGVInEzfJEBzKLIwLpVZcGE',
-      database: 'railway',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0
